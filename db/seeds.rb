24eb73a5b166bf2c79a7b1140eb2a7b5a0f44146
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-11-10 04:38:43 +0000

    フォロー機能実装前

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(email: "admin@example.com", password: "111111")
+
+require 'faker'
+10.times do |n|
+  name = Gimei.kanji
+  email = Faker::Internet.email
+  password = 'test1234TEST'
+  password_confirmation = 'test1234TEST'
+  birthday = Faker::Date.between(from: '1960-01-01', to: '2000-04-30')
+# start_time = Faker::Date.between(from: '2021-01-01', to: '2021-04-30') #期間を限定することもできます。
+  User.create!(
+    name:                   name, 
+    email:                  email,
+    introduction:           "よろしくおねがいします",
+    password:               password,
+    password_confirmation:  password_confirmation,
+    birthday:               birthday, 
+    created_at:             "2023-11-9 00:00:00",
+    updated_at:             "2023-11-9 00:00:00"
+  )
+end
