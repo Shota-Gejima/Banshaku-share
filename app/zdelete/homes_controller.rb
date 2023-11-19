4db962c493cf9a5dd0dafb5178851dcf95d77d1c
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-11-19 08:53:57 +0000

    zdelete削除

--- app/zdelete/homes_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::HomesController < ApplicationController
-
-  def top
-  end
-end
--- app/zdelete/searches_controller.rb
+++
@@ -1,7 +0,0 @@
-class Public::SearchesController < ApplicationController
-  
-  def search
-    alcohol_id = params[:alcohol_id]
-    @alcohols = Recipe.where(alcohol_id: alcohol_id)
-  end
-end
