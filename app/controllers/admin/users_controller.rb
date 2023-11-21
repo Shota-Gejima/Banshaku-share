9dab4e26a924bad4c65b673a388084bbbaa94fdc
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-11-21 13:18:27 +0000

    flashメッセージ2

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @users = User.all.page(params[:page])
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
