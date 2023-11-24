a00c845e03c1fde00e0c972b95deb5a89a0e98a7
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-11-24 01:49:36 +0000

    本番環境バグ出し前

--- app/models/making_time.rb
+++ app/models/making_time.rb
@@ -3,10 +3,10 @@ class MakingTime < ActiveHash::Base
   has_many :recipes
   
   self.data = [
-    {id: 1, name: '5分未満'},
-    {id: 2, name: '10分未満'},
-    {id: 3, name: '15分未満'},
-    {id: 4, name: '20分未満'},
+    {id: 1, name: '5分'},
+    {id: 2, name: '10分'},
+    {id: 3, name: '15分'},
+    {id: 4, name: '20分'},
     {id: 5, name: '20分以上'},
   ]
 end
