b11abdc15eb107cc62a106295c9106a9e73fa687
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-12-16 08:36:51 +0000

    [edit]turbolinks-false

--- app/javascript/top-page-button.js
+++ app/javascript/top-page-button.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   // スクロールトップボタンをクリックしたときの処理
-  $('#top a').click(function(e) {
-    $('html, body').animate({ scrollTop: 0 }, 800);
-    e.preventDefault();// ページトップへスクロール
+  $('.scroll-top-btn').on('click', function(e) {
+  	e.preventDefault(); 
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    // ページトップへスクロール
   });
-
   // ページが100ピクセルまでスクロールされたらボタンを表示
-  // $(window).scroll(function() {
-    // if ($(this).scrollTop() > 100) {
-      // $('.scroll-top-btn').fadeIn();
-    // } else {
-      // $('.scroll-top-btn').fadeOut();
-    // }
-  // });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-top-btn').fadeIn();
+    } else {
+      $('.scroll-top-btn').fadeOut();
+    }
+  });
 });
