1aa181912e3bf6de0660d34ebced54b15ccac6fc
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-12-23 10:31:04 +0000

    [edit] PF-feedback

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,8 +2,11 @@ class Public::FavoritesController < ApplicationController
   
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    favorite = current_user.favorites.new(recipe_id: @recipe.id)
-    favorite.save
+    favorite_count = Favorite.where(user_id: current_user, recipe_id: @recipe).count
+    if favorite_count == 0
+      favorite = current_user.favorites.new(recipe_id: @recipe.id)
+      favorite.save
+    end
   end
   
   def destroy
--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,8 +2,11 @@ class Public::RelationshipsController < ApplicationController
   
   def create
     @user = User.find(params[:user_id])
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
+    relationship_count = Relationship.where(following_id: current_user.id).where(follower_id: @user.id).count
+    if relationship_count == 0
+      follow = current_user.active_relationships.build(follower_id: params[:user_id])
+      follow.save
+    end
   end
 
   def destroy
